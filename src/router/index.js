680c08b0244529dfd43172596b9f8c335734cc9b
Usman <Murtala>
2020-07-01 21:37:56 +0100

    Structured components and added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "../components/Home.vue";
+import Portfolio from "../components/portfolio/Portfolio.vue";
+import Stocks from "../components/stocks/Stocks.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  { path: "/", component: Home },
+  { path: "/portfolio", component: Portfolio },
+  { path: "/stocks", component: Stocks }
 ];
 
 const router = new VueRouter({
@@ -28,3 +19,5 @@ const router = new VueRouter({
 });
 
 export default router;
+
+// 2218069590
